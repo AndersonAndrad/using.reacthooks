cf46f7807fcc366406a9d0576b272e4d95f8e049
AndersonAndrad <Anderson_Andrade_@outlook.com>
2020-05-04 13:47:15 -0300

    {UPDATE: adding useState, useEffect, useMemo, useCallback in application}

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
 function App() {
+  const [tech, setTech] = useState(['reactjs', 'reactNative']);
+  const [newTech, setNewTech] = useState('');
+
+  const handleAdd = useCallback(() => {
+    setTech([...tech, newTech]);
+    setNewTech('');
+  }, [newTech, tech]);
+
+  useEffect(() => {
+    const storageTech = localStorage.getItem('tech');
+
+    if (storageTech) {
+      setTech(JSON.parse(storageTech));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]);
+
+  const techSize = useMemo(() => tech.length, [tech]);
+
   return (
     <>
-      <h1>is running</h1>
+      <ul>
+        {tech.map((techs) => (
+          <li key={techs}>{techs}</li>
+        ))}
+      </ul>
+      <strong>You have {techSize} technologies</strong>
+      <br />
+      <input
+        value={newTech}
+        onChange={(event) => setNewTech(event.target.value)}
+      />
+      <button onClick={handleAdd}>Add</button>
     </>
   );
 }
